dfd9fa0bfe0b559b00ec16a92fd47b0d3e44b840
Antony Bourgois <antonyb@me.com>
2016-10-16 17:08:00 +0100

    adds basic .aliases and .gitgnore

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 export DOTFILES_DIR=$HOME/dotfiles
 
+ln -sfv "$DOTFILES_DIR/.aliases" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore" ~
 
